9f2868d3261af081f801a1a06467c42bd6c2e560
Matt Donahoe <matt@skydio.com>
2019-03-02 01:33:40 -0800

    update install deps

--- client/gstreamer_viewer.py
+++ client/gstreamer_viewer.py
@@ -18,7 +18,8 @@ This is useful for viewing the RTP strema from R1 with no-lag
     apt-get install gstreamer1.0-libav \
             gstreamer1.0-plugins-base \
             gstreamer1.0-plugins-good \
-            gstreamer1.0-plugins-bad
+            gstreamer1.0-plugins-bad \
+            gstreamer1.0-tools
     ```
 
 """
