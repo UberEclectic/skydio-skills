e28c61b3f6d5dba70408f60613f6725d88d9553b
Matt Donahoe <matt@skydio.com>
2019-03-25 23:06:29 -0700

    samples for Lead and Side

--- skillset/__init__.py
+++ skillset/__init__.py
@@ -1,4 +1,5 @@
 from .com_link import ComLink
+from .follow_modes import Lead, Side
 from .party_mode import PartyMode
 from .polygon_path import PolygonPath
 from .property_tour import PropertyTour
